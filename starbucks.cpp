b100b7e1227faa652e0d4a3127da1266c1fd4944
Leutrim <LeutrimD1@gmail.com>
2018-11-27 17:37:47 -0600

    added labels and image

--- starbucks.cpp
+++ starbucks.cpp
@@ -3,34 +3,36 @@
 
 main_window::main_window()
 : m_VBox(Gtk::ORIENTATION_VERTICAL),
-  m_Label1("Contents of tab 1"),
-  m_Label2("Contents of tab 2"),
-  m_Button_Quit("Quit")
+	m_Label1("Contents of tab 1"),
+	m_Label2("Contents of tab 2"),
+	m_Label3("Contents of tab 3"),
+	m_Button_Quit("Quit")
 {
-  set_title("Gtk::Notebook example");
-  set_border_width(10);
-  set_default_size(400, 200);
+	set_title("Gtk::Notebook example");
+	set_border_width(2);
+	set_default_size(743, 560);
 
 
-  add(m_VBox);
+	add(m_VBox);
 
-  //Add the Notebook, with the button underneath:
-  m_Notebook.set_border_width(10);
-  m_VBox.pack_start(m_Notebook);
-  m_VBox.pack_start(m_ButtonBox, Gtk::PACK_SHRINK);
+	//Add the Notebook, with the button underneath:
+	m_Notebook.set_border_width(3);
+	image.set("starbucks_img.jpg");
+	m_VBox.pack_start(m_Notebook);
+	m_VBox.pack_start(m_ButtonBox, Gtk::PACK_SHRINK);
 
-  m_ButtonBox.pack_start(m_Button_Quit, Gtk::PACK_SHRINK);
-  m_Button_Quit.signal_clicked().connect(sigc::mem_fun(*this,
-              &main_window::on_button_quit) );
+	m_ButtonBox.pack_start(m_Button_Quit, Gtk::PACK_SHRINK);
+	m_Button_Quit.signal_clicked().connect(sigc::mem_fun(*this, &main_window::on_button_quit));
 
-  //Add the Notebook pages:
-  m_Notebook.append_page(m_Label1, "First");
-  m_Notebook.append_page(m_Label2, "Second");
+	//Add the Notebook pages:
+	m_Notebook.append_page(image, "Home");
+	m_Notebook.append_page(m_Label1, "Order");
+	m_Notebook.append_page(m_Label2, "Apply");
+	m_Notebook.append_page(m_Label3, "Nutrition Facts");
 
-  m_Notebook.signal_switch_page().connect(sigc::mem_fun(*this,
-              &main_window::on_notebook_switch_page) );
+	m_Notebook.signal_switch_page().connect(sigc::mem_fun(*this, &main_window::on_notebook_switch_page));
 
-  show_all_children();
+	show_all_children();
 }
 
 main_window::~main_window()
@@ -39,12 +41,12 @@ main_window::~main_window()
 
 void main_window::on_button_quit()
 {
-  hide();
+	hide();
 }
 
 void main_window::on_notebook_switch_page(Gtk::Widget* /* page */, guint page_num)
 {
-  std::cout << "Switched to tab with index " << page_num << std::endl;
+	std::cout << "Switched to tab with index " << page_num << std::endl;
 
-  //You can also use m_Notebook.get_current_page() to get this index.
+	//You can also use m_Notebook.get_current_page() to get this index.
 }
--- starbucks.h
+++ starbucks.h
@@ -6,21 +6,22 @@
 class main_window : public Gtk::Window
 {
 public:
-  main_window();
-  virtual ~main_window();
+	main_window();
+	virtual ~main_window();
 
 protected:
-  //Signal handlers:
-  void on_button_quit();
-  void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
+	//Signal handlers:
+	void on_button_quit();
+	void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
 
-  //Child widgets:
-  Gtk::Box m_VBox;
-  Gtk::Notebook m_Notebook;
-  Gtk::Label m_Label1, m_Label2;
+	//Child widgets:
+	Gtk::Box m_VBox;
+	Gtk::Notebook m_Notebook;
+	Gtk::Image image;
+	Gtk::Label m_Label1, m_Label2, m_Label3;
 
-  Gtk::ButtonBox m_ButtonBox;
-  Gtk::Button m_Button_Quit;
+	Gtk::ButtonBox m_ButtonBox;
+	Gtk::Button m_Button_Quit;
 };
 
 #endif //GTKMM_EXAMPLE:WINDOW_H
