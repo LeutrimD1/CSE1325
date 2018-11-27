21a267397b2d258e46af578fca53aebce794c673
LeutrimD1 <45049970+LeutrimD1@users.noreply.github.com>
2018-11-27 13:55:05 -0600

    Update starbucks.h

--- starbucks.h
+++ starbucks.h
@@ -1,18 +1,26 @@
-#ifndef HEADER
-#define HEADER
+#ifndef GTKMM_EXAMPLEWINDOW_H
+#define GTKMM_EXAMPLEWINDOW_H
+
 #include <gtkmm.h>
-class main_window : public Gtk::Window{
-	public:
-		main_window();		//constructor
-		virtual ~main_window();	//destructor
-	protected:
-		//add stuff here
-		//signal handlers
-		//widgets
-		Gtk::Image image;
-		Gtk::Button order;
-		Gtk::Button apply;
-		Gtk::Button quit;
-		Gtk::Grid grid;
+
+class ExampleWindow : public Gtk::Window
+{
+public:
+  ExampleWindow();
+  virtual ~ExampleWindow();
+
+protected:
+  //Signal handlers:
+  void on_button_quit();
+  void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
+
+  //Child widgets:
+  Gtk::Box m_VBox;
+  Gtk::Notebook m_Notebook;
+  Gtk::Label m_Label1, m_Label2;
+
+  Gtk::ButtonBox m_ButtonBox;
+  Gtk::Button m_Button_Quit;
 };
-#endif
+
+#endif //GTKMM_EXAMPLEWINDOW_H
