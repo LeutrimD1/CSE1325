c46eecf62ef689384a06c4a4e4203c894bf0819e
LeutrimD1 <45049970+LeutrimD1@users.noreply.github.com>
2018-11-29 16:59:51 -0600

    Update starbucks.h
    added comments to make it pretty.

--- starbucks.h
+++ starbucks.h
@@ -1,24 +1,15 @@
 #ifndef GTKMM_EXAMPLEWINDOW_H
 #define GTKMM_EXAMPLEWINDOW_H
-
 #include <gtkmm.h>
-/*class RadioButtons : public Gtk::Window
-{
-	public:
-		RadioButton();
-	protected:
-		Gtk::RadioButton */
 class main_window : public Gtk::Window
 {
 public:
 	main_window();
 	virtual ~main_window();
-
 protected:
 	//Signal handlers:
 	void on_button_quit();
 	void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
-
 	//Child widgets:
 	Gtk::Box VBox;//VBox is for quit button at the bottom
 	//-----------------------tabs---------------------------
@@ -26,14 +17,13 @@ protected:
 	Gtk::Grid grid2;
 	Gtk::Grid grid3;
 	//------------------------------------------------------
-	Gtk::Notebook Notebook;
-	Gtk::RadioButton Latte_Check, Cappuccino_Check, Macchiato_Check;
-	Gtk::Image image;
-	Gtk::Label Label1, Label2, Label3;
-
+	Gtk::Notebook Notebook;//Creates the widget which contains the pages.
+	Gtk::RadioButton Latte_Check, Cappuccino_Check, Macchiato_Check;//Radio buttons for selecting the drink.
+	Gtk::Image image;//creates the image widget.
+	Gtk::Label Label1, Label2, Label3;//needs explaination.
+	//---------------------page buttons---------------------
 	Gtk::ButtonBox ButtonBox;
-	Gtk::Button quit;
+	Gtk::Button quit;//Button used to quit
 	Gtk::Button order;
 };
-
 #endif //GTKMM_EXAMPLE:WINDOW_H
