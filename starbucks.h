bcad8451c0b63450d27b3f6a00f996c3d815eb3d
Leutrim <LeutrimD1@gmail.com>
2018-11-30 02:01:21 -0600

    added some comments

--- starbucks.h
+++ starbucks.h
@@ -11,30 +11,29 @@ protected:
 	void on_button_quit();
 	void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
 	//Child widgets:
-	Gtk::Box VBox;//VBox is for quit button at the bottom
+	Gtk::Box VBox;							//VBox is for quit button at the bottom
 	//-----------------------tabs---------------------------
 	Gtk::Grid grid1;
 	Gtk::Grid grid2;
 	Gtk::Grid grid3;
 	//------------------------------------------------------
-	Gtk::Notebook Notebook;//Creates the widget which contains the pages.
+	Gtk::Notebook Notebook;						//Creates the widget which contains the pages.
 	Gtk::RadioButton Latte_Check, Cappuccino_Check, Macchiato_Check;//Radio buttons for selecting the drink.
-	Gtk::RadioButton Small_Check, Medium_Check, Large_Check; //Radio buttons for selecting the size of a drink.
-	Gtk::RadioButton Vanilla_Check, Caramel_Check, None_Check; //Radio buttons for selecting a flavor.
-	Gtk::Image image;//creates the image widget.
-	Gtk::Label Label1, Label2, Label3, Label12, Label13;//needs explaination.
+	Gtk::RadioButton Small_Check, Medium_Check, Large_Check; 	//Radio buttons for selecting the size of a drink.
+	Gtk::RadioButton Vanilla_Check, Caramel_Check, None_Check; 	//Radio buttons for selecting a flavor.
+	Gtk::Image image;						//creates the image widget.
+	Gtk::Label Label1, Label2, Label3, Label12, Label13;		//needs explaination.
 	//---------------------page buttons---------------------
-	Gtk::ButtonBox ButtonBox;
-	Gtk::Button quit;//Button used to quit
-	Gtk::Button order;
+	Gtk::ButtonBox ButtonBox;					//Holds the buttons below
+	Gtk::Button quit;						//Button used to quit
+	Gtk::Button order;						//Button used to send the order out
 	//.....................nutrition widgets...............
-	Gtk::Button Latte; //opens Latte nutrition
-	Gtk::Button Cap; //opens Cappaccino nutrition
-	Gtk::Button Machi; //opens Macchiato nutrition
+	Gtk::Button Latte; 					//opens Latte nutrition
+	Gtk::Button Cap; 					//opens Cappaccino nutrition
+	Gtk::Button Machi;					//opens Macchiato nutrition
 
-	void on_latte(); //opens Latte
-	void on_cap(); //opens Cap
-	void on_machi(); //opens Machi
-
-};//test
+	void on_latte(); 				//opens Latte
+	void on_cap(); 					//opens Cap
+	void on_machi(); 				//opens Machi
+};
 #endif //GTKMM_EXAMPLE:WINDOW_H
